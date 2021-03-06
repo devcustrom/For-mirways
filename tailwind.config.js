15f5601054f909e13d2f031588f353fa91487f03
devcustrom <devcustrom@gmail.com>
2021-03-06 21:46:38 +0300

    Лендинг за 5 минут

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [
+    '*.html',
+    'js/*.js'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}
